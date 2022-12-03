af58f4db571a77d2b59f96416048d899f12a9496
Tellurnoid <105495041+Tellurnoid@users.noreply.github.com>
2022-12-04 00:48:23 +0900

    Update ili9341-FunctionTest.ino

--- ili9341-FunctionTest.ino
+++ ili9341-FunctionTest.ino
@@ -3,16 +3,22 @@ Don't forget editing "User_Setup.h" in the TFT_eSPI.h library.
 My Instagram https://www.instagram.com/tellurnoid/
 */
 
-
 #include "SPI.h"
 #include "TFT_eSPI.h"
+#include "xbm.h"   
 #define TFT_GREY 0x7BEF
+
 TFT_eSPI myGLCD = TFT_eSPI();       // Invoke custom library
+
+int startPx;
+int startPy;
 void setup()
 {
+// Setup the LCD
   myGLCD.init();
-  myGLCD.setRotation(1);
-  myGLCD.fillScreen(TFT_BLACK);
+  myGLCD.setRotation(3);
+  myGLCD.fillScreen(TFT_WHITE);
+  myGLCD.drawXBitmap(0, 0, logo, logoWidth, logoHeight, TFT_BLACK);
   pinMode(TFT_BL, OUTPUT);
   digitalWrite(TFT_BL, HIGH);
   myGLCD.setTextDatum(TC_DATUM);
@@ -20,80 +26,46 @@ void setup()
   myGLCD.setTextColor(TFT_WHITE,TFT_DARKGREY);
   myGLCD.drawCentreString("Tellurnoid", 160, 93,2);
   myGLCD.drawCentreString("Let's see functions!", 160, 119,2);
-  delay(1500);
 }
 int y;
+int radius = 1;
 void loop(){
   int buf[318];
-  myGLCD.fillScreen(TFT_DARKGREY);
-  //myGLCD.fillRect(0, 0, 160, 70,TFT_DARKGREY);
-  myGLCD.setTextDatum(TC_DATUM);
-  myGLCD.setTextColor(TFT_BLUE,TFT_DARKGREY);
-  myGLCD.drawCentreString("y=1/2*x+50", 65, 9,4);
-  for(int x=0; x<240; x++){
-  myGLCD.drawPixel(x,1/2*x+50,TFT_CYAN);
-  delay(10);
-  }
-
-  delay(1000);
-  myGLCD.fillScreen(TFT_DARKGREY);
-  myGLCD.setTextDatum(TC_DATUM);
-  myGLCD.setTextColor(TFT_BLUE,TFT_DARKGREY);
-  myGLCD.drawCentreString("y=2*x+50", 65, 10,4);
+  SetMap();
+  myGLCD.drawCentreString("y=-2*x", 60, 10,4);
   for(int x=0; x<140; x++){
-  myGLCD.drawPixel(x,2*x+50,TFT_CYAN);
-  delay(10);
-  }
-
+   y=2*x;
+   myGLCD.fillCircle(x+160,y+120,radius,TFT_GREEN);
+   myGLCD.fillCircle(160-x,120-y,radius,TFT_GREEN);
+   delay(10);
+   }
   delay(1000);
-  myGLCD.fillScreen(TFT_DARKGREY);
-  myGLCD.setTextDatum(TC_DATUM);
-  myGLCD.setTextColor(TFT_BLUE,TFT_DARKGREY);
-  myGLCD.drawCentreString("y=x/2+50", 65, 10,4);
-  for(int x=0; x<350; x++){
-  myGLCD.drawPixel(x,x/2+50,TFT_CYAN);
-  delay(10);
-  }
-
-  delay(1000);
-  myGLCD.fillScreen(TFT_DARKGREY);
-  myGLCD.setTextDatum(TC_DATUM);
-  myGLCD.setTextColor(TFT_BLUE,TFT_DARKGREY);
-  myGLCD.drawCentreString("y=x^2", 65, 10,4);
+  
+  SetMap();
+  myGLCD.drawCentreString("y=-x^2", 60, 10,4);
   for(int x=0; x<50; x++){
-  y = pow(x,2);
-  myGLCD.drawPixel(x,y,TFT_GREEN);
-  delay(20);
-  }
-  for(int x=0; x<70; x++){
-  y = pow(x,2);
-  myGLCD.drawPixel(x+50,y,TFT_GREEN);
-  delay(20);
-  }
-  for(int x=0; x<70; x++){
-  y = pow(x,2);
-  myGLCD.drawPixel(x+100,y,TFT_GREEN);
-  delay(20);
-  }
-  for(int x=0; x<70; x++){
-  y = pow(x,2);
-  myGLCD.drawPixel(x+150,y,TFT_GREEN);
-  delay(20);
-  }
-  for(int x=0; x<70; x++){
-  y = pow(x,2);
-  myGLCD.drawPixel(x+200,y,TFT_GREEN);
-  delay(20);
-  }
-  for(int x=0; x<70; x++){
-  y = pow(x,2);
-  myGLCD.drawPixel(x+250,y,TFT_GREEN);
-  delay(20);
-  }
-  for(int x=0; x<70; x++){
-  y = pow(x,2);
-  myGLCD.drawPixel(x+300,y,TFT_GREEN);
-  delay(20);
-  }
-delay(1000);
+   y = pow(x,2);
+   myGLCD.fillCircle(x+160,y+120,radius,TFT_GREEN);
+   myGLCD.fillCircle(160-x,120-y,radius,TFT_GREEN);
+   delay(20);
+   }
+  delay(1000);
+}
+
+void SetMap(){
+  myGLCD.fillScreen(TFT_BLACK);
+  myGLCD.setTextDatum(TC_DATUM);
+  myGLCD.setTextColor(TFT_WHITE,TFT_BLACK);
+  myGLCD.drawLine(0, 119, 320, 119,TFT_DARKGREEN);
+  myGLCD.drawLine(0, 120, 320, 120,TFT_DARKGREEN);
+  myGLCD.drawLine(0, 121, 320, 121,TFT_DARKGREEN);
+
+  myGLCD.drawLine(159, 0, 159, 240,TFT_DARKGREEN);
+  myGLCD.drawLine(160, 0, 160, 240,TFT_DARKGREEN);
+  myGLCD.drawLine(161, 0, 161, 240,TFT_DARKGREEN);
+
+for(int i=0; i<12; i++){myGLCD.drawLine(0, i*20, 320, i*20,TFT_DARKGREEN);}
+                        myGLCD.drawLine(0, 239, 320, 239,TFT_DARKGREEN);
+for(int i=0; i<16; i++){myGLCD.drawLine(i*20, 0, i*20, 240,TFT_DARKGREEN);}
+                        myGLCD.drawLine(319, 0, 319, 240,TFT_DARKGREEN);
 }
